b6c74c428e7ada0c1fe427d50d8d4402efe6793c
Kyle Gentle <kylegentle@gmail.com>
2022-12-06 15:58:06 -0500

    Day 6 Python

---
+++ 06 - Tuning Trouble/part2.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+from collections import deque
+from typing import Deque
+
+SEGMENT_SIZE: int = 14
+
+
+def main() -> None:
+    position = 0
+    segment = deque(maxlen=SEGMENT_SIZE)
+
+    with open("input.txt", "r") as f:
+        for line in f:
+            for c in line.strip():
+                segment.append(c)
+                position += 1
+
+                if len(set(segment)) == SEGMENT_SIZE:
+                    return print(position)
+
+
+if __name__ == "__main__":
+    main()
